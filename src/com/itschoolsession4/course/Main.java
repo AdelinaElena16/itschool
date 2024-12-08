4b13e2fe166707bfc0bb36162b0f168ee2c12b72
AdelinaElena16 <adelinaelena910@yahoo.com>
2024-12-08 22:34:41 +0200

    add salut

--- src/com/itschoolsession4/course/Main.java
+++ src/com/itschoolsession4/course/Main.java
@@ -3,6 +3,7 @@ package com.itschoolsession4.course;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("salut");
     }
 }
 
